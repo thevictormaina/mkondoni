f5a23b954a7e081b0a6e0d958033f51624e9365b
kev127 <kelvinkeya125@gmail.com>
2020-11-04 11:29:47 +0300

    add class voter

---
+++ app/models.py
@@ -0,0 +1,7 @@
+class Voter(UserMixin,db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    national_id = db.Column(db.Integer)
+    passport = db.Column(db.Integer)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    location = db.Column(db.String(255))
