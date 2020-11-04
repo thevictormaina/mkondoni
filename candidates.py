ffdaed1d2a3f833fe217302abbc49c2a2d8cc607
mishel254 <sonimichie@gmail.com>
2020-11-04 12:34:07 +0300

    Presidential candidates

---
+++ candidates.py
@@ -0,0 +1,61 @@
+from app.models import PresidentialS,Gubernatorials,Senetorials
+from app import db
+
+Presidentials = [
+    Presidential(first_name = "John", last_name="Joe",location="Kenya",votes=0),
+    Presidential(first_name = "Jane", last_name="Joe",location="Kenya",votes=0),
+    Presidential(first_name = "Uhuru", last_name="Kenyatta",location="Kenya",votes=0), 
+    Presidential(first_name = "Raila", last_name="Odinga",location="Kenya",votes=0)
+    Presidential(first_name = "William", last_name="Ruto",location="Kenya",votes=0)
+    Presidential(first_name = "Kalonzo", last_name="Musyoka",location="Kenya",votes=0,)
+]
+
+def presidentials_votes(presidentials):
+    for Presidential in presidentials:
+        db.session.add(Presidential)
+        db.session.commit()
+        print("\n", voter, "\n")
+
+add_presidents(Presidential)
+
+ Gubernatorials = [
+     gubernatorial(first_name = "Lee", last_name="Kinyanjui",location="Nyeri",votes=0),
+     gubernatorial(first_name = "Samuel", last_name="Kamau",location="Nairobi",votes=0),
+     gubernatorial(first_name = "Agnes", last_name="Joe",location="Kisumu",votes=0), 
+     gubernatorial(first_name = "Andrew", last_name="Onyango",location="Mombasa",votes=0)
+     gubernatorial(first_name = "Sandra", last_name="Kyalo",location="Kwale",votes=0)
+     gubernatorial(first_name = "Nancy", last_name="Kamulu",location="Nairobi",votes=0,)
+     gubernatorial(first_name = "Mark", last_name="Owiti",location="Mombasa",votes=0,)
+     gubernatorial(first_name = "Amina", last_name="Kitui",location="Nyeri",votes=0,)
+     gubernatorial(first_name = "Luca", last_name="Kitui",location="Kwale",votes=0,)
+     gubernatorial(first_name = "Felista", last_name="Toms",location="Kisumu",votes=0,)
+]
+
+def gubernatorials_votes(gubernatorial):
+    for gubernatorial in gubernatorials:
+        db.session.add(gubernatorial)
+        db.session.commit()
+        print("\n", voter, "\n")
+
+add_governors(gubernatorial)
+
+SenatorialS = [
+     senatorial(first_name = "Alice", last_name="Kipkoech",location="Nyeri",votes=0),
+     senatorial(first_name = "Jane", last_name="Oyando",location="Nairobi",votes=0),
+     senatorial(first_name = "Michael", last_name="Mwaura",location="Kisumu",votes=0), 
+     senatorial(first_name = "Andrew", last_name="Juma",location="Mombasa",votes=0)
+     senatorial(first_name = "Sandra", last_name="Mwikali",location="Kwale",votes=0)
+     senatorial(first_name = "Lisa", last_name="Ali",location="Nairobi",votes=0,)
+     senatorial(first_name = "Kipkurui", last_name="Mercy",location="Mombasa",votes=0,)
+     senatorial(first_name = "Sandy", last_name="Maalim",location="Nyeri",votes=0,)
+     senatorial(first_name = "Abdi", last_name="Hussein",location="Kwale",votes=0,)
+     senatorial(first_name = "Erick", last_name="Boaz",location="Kisumu",votes=0,)
+]
+
+def senetarial_votes(senetorial):
+    for senetorial in senetorials:
+        db.session.add(senetorial)
+        db.session.commit()
+        print("\n", voter, "\n")
+
+add_senetors(senetarial)
--- create_voters.py
+++ create_voters.py
@@ -1,8 +1,16 @@
 from app.models import Voter
 from app import db
 
-voters = [Voter(national_id = "10345343", passport="", first_name = "John", last_name="Joe"), Voter(national_id = "34534454", passport="", first_name = "Jane", last_name="Joe"), Voter(national_id = "45656764", passport="", first_name = "Michael", last_name="Joe"), Voter(national_id = "23434565", passport="", first_name = "Andrew", last_name="Does")
+voters = [
+    Voter(national_id = "10345343", first_name = "John", last_name="Joe",location="Nairobi"),
+    Voter(national_id = "34534454", first_name = "Jane", last_name="Joe",location="Mombasa"),
+    Voter(national_id = "45656764", first_name = "Michael", last_name="Joe",location="Kisumu"), 
+    Voter(national_id = "23434565", first_name = "Andrew", last_name="Dose",location="Kwale")
+    Voter(national_id = "93434565", first_name = "Sandra", last_name="Sandy",location="Nyeri")
+    Voter(national_id = "53434565", first_name = "Lisa", last_name="Kitui",location="kitui")
 ]
+
+
 
 def add_voters(voters):
     for voter in voters:
