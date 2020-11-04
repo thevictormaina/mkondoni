48e54dfd9d54c6412ce66e0054d72a59bb67b155
kev127 <kelvinkeya125@gmail.com>
2020-11-04 16:59:56 +0300

    add test case president instance

--- tests/test_president.py
+++ tests/test_president.py
@@ -12,3 +12,26 @@ class TestPresident(unittest.TestCase):
         db.create_all()
 
         self.president = President(id=" ", first_name=" ", last_name=" ", location=" ", votes=" ")
+
+     def tearDown(self):
+        """
+        Runs after each test
+        """
+        db.session.commit()
+        db.drop_all()
+
+    def test_president_instance(self):
+        """
+        Test cases to check if president instance is created and commited correctly
+        """
+        db.session.add(self.president)
+        db.session.commit()
+
+        president = President.query.filter_by(first_name = " ").first()
+
+        self.assertEqual(president.id,1)
+        self.assertEqual(president.first_name, " ")
+        self.assertEqual(president.last_name, " ")
+        self.assertEqual(president.location, " ")
+        self.assertEqual(president.votes,0)
+   
