97bae99ed0fd71639bb9fb68adcb3ef67614bae7
Victor Maina <contact@victormaina.com>
2020-11-04 17:57:16 +0300

    added commas in create_voters.py

--- create_voters.py
+++ create_voters.py
@@ -5,8 +5,8 @@ voters = [
     Voter(national_id = "10345343", first_name = "John", last_name="Joe",location="Nairobi"),
     Voter(national_id = "34534454", first_name = "Jane", last_name="Joe",location="Mombasa"),
     Voter(national_id = "45656764", first_name = "Michael", last_name="Joe",location="Kisumu"), 
-    Voter(national_id = "23434565", first_name = "Andrew", last_name="Dose",location="Kwale")
-    Voter(national_id = "93434565", first_name = "Sandra", last_name="Sandy",location="Nyeri")
+    Voter(national_id = "23434565", first_name = "Andrew", last_name="Dose",location="Kwale"),
+    Voter(national_id = "93434565", first_name = "Sandra", last_name="Sandy",location="Nyeri"),
     Voter(national_id = "53434565", first_name = "Lisa", last_name="Katua",location="kitui")
 ]
 
