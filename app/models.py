3f490157df40897761b738991dad908a31d188a3
kev127 <kelvinkeya125@gmail.com>
2020-11-04 11:37:57 +0300

    add class governor

--- app/models.py
+++ app/models.py
@@ -30,3 +30,9 @@ class Sentor(db.Model):
         db.session.add(self)
         db.session.commit()
 
+class Governor(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    location = db.Column(db.String(255))
+    votes = db.Column(db.Integer)
