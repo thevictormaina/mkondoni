eb194679840926488d3cc9a223a93074c50d4b03
kev127 <kelvinkeya125@gmail.com>
2020-11-04 17:10:44 +0300

    add test case for voter instance

--- tests/tests_voter.py
+++ tests/tests_voter.py
@@ -12,3 +12,28 @@ class TestVoter(unittest.TestCase):
         db.create_all()
 
         self.voter = Voter(id=" ", national_id=" ", passport=" ", first_name=" ", last_name=" ", location=" ")
+
+     def tearDown(self):
+        """
+        Runs after each test
+        """
+        db.session.commit()
+        db.drop_all()
+
+    def test_voter_instance(self):
+        """
+        Test cases to check if voter instance is created and commited correctly
+        """
+        db.session.add(self.governor)
+        db.session.commit()
+
+        voter = Voter.query.filter_by(first_name = " ").first()
+
+        self.assertEqual(voter.id,1)
+        self.assertEqual(voter.national_id)
+        self.assertEqual(voter.passport)
+        self.assertEqual(voter.first_name, " ")
+        self.assertEqual(voter.last_name, " ")
+        self.assertEqual(voter.location, " ")
+        self.assertEqual(voter.voted)
+       
