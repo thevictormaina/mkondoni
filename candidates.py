35c23413827b1792e4267265ea33b1d349047ef5
mishel254 <sonimichie@gmail.com>
2020-11-04 12:39:02 +0300

    gubernatotial candidates

--- candidates.py
+++ candidates.py
@@ -18,3 +18,24 @@ def presidentials_votes(presidentials):
 
 add_presidents(Presidential)
 
+ Gubernatorials = [
+     gubernatorial(first_name = "Lee", last_name="Kinyanjui",location="Nyeri",votes=0),
+     gubernatorial(first_name = "Samuel", last_name="Kamau",location="Nairobi",votes=0),
+     gubernatorial(first_name = "Agnes", last_name="Joe",location="Kisumu",votes=0), 
+     gubernatorial(first_name = "Andrew", last_name="Onyango",location="Mombasa",votes=0)
+     gubernatorial(first_name = "Sandra", last_name="Kyalo",location="Kwale",votes=0)
+     gubernatorial(first_name = "Nancy", last_name="Kamulu",location="Nairobi",votes=0,)
+     gubernatorial(first_name = "Mark", last_name="Owiti",location="Mombasa",votes=0,)
+     gubernatorial(first_name = "Amina", last_name="Kitui",location="Nyeri",votes=0,)
+     gubernatorial(first_name = "Luca", last_name="Kitui",location="Kwale",votes=0,)
+     gubernatorial(first_name = "Felista", last_name="Toms",location="Kisumu",votes=0,)
+]
+
+def gubernatorials_votes(gubernatorial):
+    for gubernatorial in gubernatorials:
+        db.session.add(gubernatorial)
+        db.session.commit()
+        print("\n", voter, "\n")
+
+add_governors(gubernatorial)
+
