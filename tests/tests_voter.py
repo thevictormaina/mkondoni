e17ccb07a3c7de4e82c57da1214206f1680e38d1
kev127 <kelvinkeya125@gmail.com>
2020-11-04 11:58:13 +0300

    add test case for voter

---
+++ tests/tests_voter.py
@@ -0,0 +1,14 @@
+import unittest
+from app.models import Voter,President,Senator,Governor
+
+class TestVoter(unittest.TestCase):
+    """
+    Class for testing Voter properties and methods. Inherits from unittest.TestCase
+    """
+    def setUp(self):
+        """
+        Runs before each test
+        """
+        db.create_all()
+
+        self.voter = Voter(id=" ", national_id=" ", passport=" ", first_name=" ", last_name=" ", location=" ")
