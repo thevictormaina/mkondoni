d7121317e1d89c769fb4637612aae7abc633028d
kev127 <kelvinkeya125@gmail.com>
2020-11-04 17:06:50 +0300

    add test case senator instance

--- tests/test_senator.py
+++ tests/test_senator.py
@@ -12,3 +12,26 @@ class TestSenator(unittest.TestCase):
         db.create_all()
 
         self.senator = Senator(id=" ", first_name=" ", last_name=" ", location=" ", votes=" ")
+
+     def tearDown(self):
+        """
+        Runs after each test
+        """
+        db.session.commit()
+        db.drop_all()
+
+    def test_senator_instance(self):
+        """
+        Test cases to check if senator instance is created and commited correctly
+        """
+        db.session.add(self.senator)
+        db.session.commit()
+
+        senator = Senator.query.filter_by(first_name = " ").first()
+
+        self.assertEqual(senator.id,1)
+        self.assertEqual(senator.first_name, " ")
+        self.assertEqual(senator.last_name, " ")
+        self.assertEqual(senator.location, " ")
+        self.assertEqual(senator.votes,0)
+   
