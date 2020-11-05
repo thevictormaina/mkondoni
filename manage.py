2dc60436c56482185507a195ab93e5f3748964a3
Victor Maina <contact@victormaina.com>
2020-11-05 22:25:59 +0300

    add party logos

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate, MigrateCommand
 from commit_data import commit_data
 
 # Create app instance
-app = create_app("production")
+app = create_app("development")
 
 manager = Manager(app)
 manager.add_command("server", Server)
