59683ef144061c2307fe07ddeffc242059e0b82b
felkiriinya <felkiriinya@gmail.com>
2020-11-05 16:19:31 +0300

    added profile pic paths

--- commit_data.py
+++ commit_data.py
@@ -17,10 +17,10 @@ deputies = [Deputy(first_name="William", last_name="Ruto", location="Kenya",pres
 
 governors = [
     Governor(first_name="Lee", last_name="Kinyanjui",
-             location="Nakuru", votes=0, party_name="Jubilee"),
+             location="Nakuru", votes=0, party_name="Jubilee",profile_pic_path = "/app/static/images/governors/lee.jpeg"),
     Governor(first_name="Mike", last_name="Mbuvi",
-             location="Nairobi", votes=0, party_name="Jubilee"),
-    Governor(first_name="Josphat", last_name="Nanok", location="Turkana", votes=0, party_name="ODM")
+             location="Nairobi", votes=0, party_name="Jubilee",profile_pic_path = "/app/static/images/governors/ruto.jpg"),
+    Governor(first_name="Josphat", last_name="Nanok", location="Turkana", votes=0, party_name="ODM",profile_pic_path = "/app/static/images/governors/josphat.jpg")
    
 ]
 
