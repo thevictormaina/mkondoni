d8efa9cca8135a665769a438be0c6c3c509eaae1
Victor Maina <contact@victormaina.com>
2020-11-05 19:26:56 +0300

    add paths to commit_data.py

--- commit_data.py
+++ commit_data.py
@@ -9,15 +9,15 @@ voters = [Voter(national_id="12345678", first_name="John", last_name="Joe"),
           Voter(national_id="34567891", first_name="Andrew", last_name="Does")]
 
 
-presidents = [President(first_name="Uhuru", last_name="Kenyatta", location="Kenya", votes=0, party_name="Jubilee", profile_pic_path="app/static/images"), President(first_name="Raila", last_name="Odinga", location="Kenya", votes=0, party_name="ODM")
+presidents = [President(first_name="Uhuru", last_name="Kenyatta", location="Kenya", votes=0, party_name="Jubilee", profile_pic_path = "../../static/images/presidents/Uhuru_Kenyatta_1.jpg"), President(first_name="Raila", last_name="Odinga", location="Kenya", votes=0, party_name="ODM",  profile_pic_path = "../../static/images/deputies/raila.jpg")
     
 ]
 
 deputies = [Deputy(first_name="William", last_name="Ruto", location="Kenya",president_id=1, profile_pic_path = "../../static/images/deputies/ruto.jpg"), Deputy(first_name="Kalonzo", last_name="Musyoka", location="Kenya",president_id=2, profile_pic_path = "../../static/images/deputies/kalonzo.jpg")]
 
 governors = [
-    Governor(first_name="Lee", last_name="Kinyanjui", location="Nakuru", votes=0, party_name="Jubilee", profile_pic_path = "../../static/images/senators/lee.jpg"), Governor(first_name="Mike", last_name="Mbuvi",location="Nairobi", votes=0, party_name="Jubilee"),
-    Governor(first_name="Josphat", last_name="Nanok", location="Turkana", votes=0, party_name="ODM")
+    Governor(first_name="Lee", last_name="Kinyanjui", location="Nakuru", votes=0, party_name="Jubilee", profile_pic_path = "../../static/images/governors/lee.jpg"), Governor(first_name="Mike", last_name="Mbuvi",location="Nairobi", votes=0, party_name="Jubilee", profile_pic_path = "../../static/images/governors/mike.jpg"),
+    Governor(first_name="Josphat", last_name="Nanok", location="Turkana", votes=0, party_name="ODM", profile_pic_path = "../../static/images/governors/josphat.jpg")
    
 ]
 
@@ -32,8 +32,8 @@ senators = [
 ]
 
 parties =[
-        Party(party_name="ODM",party_logo_path=" "),
-        Party(party_name="Jubilee",party_logo_path=" ")
+        Party(party_name="ODM",party_logo_path="../../static/images/odm.png"),
+        Party(party_name="Jubilee",party_logo_path="../../static/images/jubilee.png")
 ]
 
 
